082bff652fc7decc283eaa71233dac0cd94f5b32
GvidasArnas <gvidas.darbai@gmail.com>
2022-06-16 11:33:36 +0300

    changes

--- project/polls/views.py
+++ project/polls/views.py
@@ -17,21 +17,18 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def site(request):
-    # response = HttpResponse("Here's the text of the web page.")
-    # response = HttpResponse("Text only, please.", content_type="text/plain")
-    # response = HttpResponse(b'Bytestrings are also accepted.')
-    now = datetime.now()
-    insertable.objects.create(method = request.method, path = request.path, date = now.strftime("%m/%Y"),company_id = "companyz", time =now.strftime("%H:%M:%S"),day= now.strftime("%d"),client_id = 2)
-   
-    return redirect('/site/company')
-
-def locale(request):
-    # response = HttpResponse("Here's the text of the web page.")
-    # response = HttpResponse("Text only, please.", content_type="text/plain")
-    # response = HttpResponse(b'Bytestrings are also accepted.')
-    output = _("Welcome to my site.")
-    return HttpResponse(settings.LOCALE_PATHS)
+    if request.method =='POST':
+        cid=request.POST.get("company","")
+        uid=request.POST.get("date",)
+        now = datetime.now()
+        insertable.objects.create(method = request.method, path = request.path, date = now.strftime("%m/%Y"),company_id = cid, time =now.strftime("%H:%M:%S"),day= now.strftime("%d"),client_id = uid)
+        return redirect('./company')  
+    else: return render(request, 'login.html')
     
+def locale(request):
+    output = _("Welcome to my site.")
+    return HttpResponse(settings.LOCALE_PATHS) 
+
 def company(request):
     cid=request.POST.get("company","")
     date=request.POST.get("date","")
